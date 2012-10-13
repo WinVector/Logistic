47779f5e8a7d143ce51af05805bc6d6c5a5075dd
John Mount <jmount@win-vector.com>
2012-10-13 11:42:29 -0700

    working around jar util not taking a list (no theory why that is happening)

--- export/mkjar.bash
+++ export/mkjar.bash
@@ -9,7 +9,20 @@ cd ..
 SRCLIST=`find src -type f`
 popd
 
-jar cmvf MANIFEST.MF ../WinVectorLogistic.Hadoop0.20.2.jar -C ../bin com/winvector -C .. src/com/winvector -C ../../SQLScrewdriver/bin $BINLIST -C ../../SQLScrewdriver $SRCLIST -C ../../Colt-1.2.0/bin cern -C ../../Colt-1.2.0 src/cern 
+
+jar cmvf MANIFEST.MF ../WinVectorLogistic.Hadoop0.20.2.jar -C ../bin com/winvector -C .. src/com/winvector -C ../../Colt-1.2.0/bin cern -C ../../Colt-1.2.0 src/cern 
+
+# no idea why jar util can't take a list
+for fi in $BINLIST
+do
+jar uvf ../WinVectorLogistic.Hadoop0.20.2.jar -C ../../SQLScrewdriver/bin $fi
+done
+
+for fi in $SRCLIST
+do
+jar uvf ../WinVectorLogistic.Hadoop0.20.2.jar -C ../../SQLScrewdriver $fi
+done
+
 
 
 
